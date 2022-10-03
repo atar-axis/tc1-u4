ddaca0b3d4746662416cd3e76426eec34c7dbbef
2Torr <ejouellette@gmail.com>
2022-10-02 20:48:34 -0400

    Improved button debounce method, direct sampling for avr_online input, changed system_tick to 5ms, changed back to Moore machine, added comments.

--- tc1-u4/main.c
+++ tc1-u4/main.c
@@ -23,8 +23,9 @@
 #define ONLINE  3
 
 char state;
-unsigned long int state_start_time;
-
+// UINT_MAX = 0xFFFF; system_tick with rollover correction supports up to 2 * UINT_MAX * 5ms = 655 seconds
+// ULONG_MAX = 0xFFFFFFFF; system_tick with rollover correction supports up to 2 * ULONG_MAX * 5ms = 248.5 days
+unsigned long int state_start_time, wait_begin_time;
 volatile unsigned long int system_tick;
 
 volatile bit button_changed;
@@ -53,36 +54,45 @@ void timer0_ISR(void) interrupt 1
 	 * [RL_TH0, RL_TL0] is "65536 - 1000", then a system-tick would be
 	 * 12 MHz / 12 / 1000 = 1 kHz.
 	 *
-	 * Therefore a system tick would take 1ms
+	 * Therefore, a system tick would take 1ms.
+	 *
+	 * For a 5ms system tick, [RL_TH0, RL_TL0] is "65536 - 5000". Then 
+	 * a system-tick would be
+	 * 12 MHz / 12 / 5000 = 200 Hz.
 	 */
 }
 
 void timer2_ISR(void) interrupt 12 using 1
 {
-	// TODO: Way too long for an ISR...
+	// Improved ISR for debouncing BUTTON and syncing AVR_ONLINE inputs...
+	// called every 5ms
 
-	static float avg_button = 10;
-	static float avg_avr_online = 10;
+	static char button_history = 0xFF; // initialized to history of unpressed button
+	
+	bit new_button_state = HIGH; // default unpressed button state
+	bit new_avr_online_state = LOW; // default avr_online state
 
-	bit new_button_state;
-	bit new_avr_online_state;
-
-	// Button are debounced using exponential moving average
-	// avg = alpha * avg + (1 - alpha) * input, alpha < 1
-	// TODO: inefficient, replace
-	avg_button     = 0.9 * avg_button + 0.1 * (I_BUTTON * 10);
-	avg_avr_online = 0.9 * avg_avr_online + 0.1 * (I_AVR_ONLINE * 10);
-
-	(avg_button > 5) ? (new_button_state = HIGH) : (new_button_state = LOW);
-	(avg_avr_online > 5) ? (new_avr_online_state = HIGH) : (new_avr_online_state = LOW);
-
-	if (new_button_state != button_state) {
+	// Button is debounced using shift register history (8 * 5ms = 40ms) to accomodate most switches.
+	// avr_online input is simply sampled.
+	
+	button_history = (button_history << 1) | I_BUTTON;
+	
+	if (button_history == 0xFF) 
+		new_button_state = HIGH;
+	else if (button_history == 0x00)
+		new_button_state = LOW;
+	
+	if (new_button_state != button_state) { // debounced BUTTON has changed state
 		button_state = new_button_state;
 		button_changed = TRUE;
-	} else {
+	} 
+	else {
 		button_changed = FALSE;
 	}
 
+	
+	new_avr_online_state = I_AVR_ONLINE;
+	
 	if (new_avr_online_state != avr_online_state) {
 		avr_online_state = new_avr_online_state;
 	}
@@ -124,8 +134,8 @@ void setup()
 	// Timer 0
 	TMOD &= 0xF0;     // T/C10 Mode 0, 16Bit, Auto Reload
 	CLR_BIT(AUXR, 7); // Timer 0 in 12T Mode
-	TH0 = (65536 - 1000) / 256; // Reload values after overflow, High value
-	TL0 = (65536 - 1000) % 256; // Low value
+	TH0 = (65536 - 5000) / 256; // Reload values after overflow, High value
+	TL0 = (65536 - 5000) % 256; // Low value
 
 	// Timer 2
 	AUXR |= 0x04; //Timer clock is 1T mode
@@ -190,16 +200,14 @@ void main()
 			break;
 
 		case BOOT:
-			O_BOOST = HIGH; // powering the booster, avr, 5v rail
-			// O_AVR_BUTTON = button_state; avoid setting button here in case of debounce issue
+			O_BOOST = HIGH; // powering the booster, AVR, 5V rail
+			O_AVR_BUTTON = button_state; // safe to set button here since it's debounced
 
 			if (avr_online_state == HIGH){ // received ACK from AVR
-				O_AVR_BUTTON = button_state;
 				transition_to_state(ONLINE);
 			}
 
-			if (ticks_since(state_start_time) > 1000){ // AVR timeout
-				O_AVR_BUTTON = button_state;
+			if (ticks_since(state_start_time) > 200){ // 1s timeout for AVR response
 				transition_to_state(SLEEP);
 			}
 
@@ -208,8 +216,10 @@ void main()
 		case ONLINE:
 			O_AVR_BUTTON = button_state;
 
-			if (avr_online_state == LOW) { // AVR going offline
-				O_BOOST = LOW; // turn off boost converter ASAP
+			if (avr_online_state == LOW) { // AVR gone offline
+				wait_begin_time = system_tick;
+				while (ticks_since(wait_begin_time) < 100) {} // wait 0.5s to display "Bye!" message before turning off AVR 5V rail.
+				
 				transition_to_state(SLEEP);
 			}
 
